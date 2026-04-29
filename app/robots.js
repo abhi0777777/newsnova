cfad524956a4b1d99153366f7485f1e055820493
abhi0777777 <abhisheklikhar587@gmail.com>
2026-04-29 20:09:05 +0530

    fix robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,8 +1,10 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
   };
 }
